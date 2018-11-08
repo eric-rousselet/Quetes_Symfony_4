e88f936898e6cf8be1370fa4dd903a9642b52d19
Eric Rousselet <rousselet.eric@gmail.com>
2018-11-08 09:55:23 +0100

    fin de quete3

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder21
+ * Date: 08/11/18
+ * Time: 09:48
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+
+}
